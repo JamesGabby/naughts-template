2b03805e6b0f556a4b4aee712f8669bb68bbff04
JamesGabby <jamesgabbitus@gmail.com>
2023-04-12 12:51:09 +0100

    calculate the winner

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     const handleClick = (i) => {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
             return
         }
 
@@ -22,6 +22,30 @@ export default function Board() {
         setSquares(nextSquares)
         setXIsNext(!xIsNext)
     }
+
+    const calculateWinner = (squares) => {
+        const winningLines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let i = 0; i < winningLines.length; i++) {
+            const [a, b, c] = winningLines[i]
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a]
+            }
+        }
+
+        return null
+    }
+
+    
 
     return (
         <div>
