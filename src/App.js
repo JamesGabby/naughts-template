915371bc581467d92a4178492013bcf38e1b88af
JamesGabby <jamesgabbitus@gmail.com>
2023-04-14 14:22:01 +0100

    add pointer cursor to button

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)])
     const [currentMove, setCurrentMove] = useState(0)
     const currentSquares = history[currentMove]
+    const squareValues = currentSquares.filter(squareValue => squareValue !== null)
 
     const handlePlay = (nextSquares) => {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
@@ -84,8 +85,6 @@ export default function Game() {
         setCurrentMove(nextHistory.length - 1)
         setXIsNext(!xIsNext)
     }
-
-    const squareValues = currentSquares.filter(squareValue => squareValue !== null)
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove)
